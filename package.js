7f7b81d4c2a5448c8b75d0be85d1fd6ae4827338
Morten Henriksen <mh@gi2.dk>
2014-09-05 19:54:28 +0200

    0.9.1

--- package.js
+++ package.js
@@ -1,23 +1,25 @@
 Package.describe({
-  name: "ejson-minimax",
+  name: "ground:minimax",
   version: '0.0.9',
   summary: "Adds MiniMax.minify/maxify/parse/stringify making compress and decompress objects"
 });
 
 Package.on_use(function (api) {
+  api.versionsFrom && api.versionsFrom('METEOR@0.9.1');
+
   // TODO: remove underscore deps _.each (used once)
   api.use([
     'underscore',
     'ejson'
   ], ['client', 'server']);
-  api.use(['dictionary'], ['client', 'server']);
+  api.use(['ground:dictionary@0.0.0'], ['client', 'server']);
   api.export('MiniMax');
 
   api.add_files('ejson.minimax.js', ['client', 'server']);
 });
 
 Package.on_test(function (api) {
-  api.use('ejson-minimax', ['client', 'server']);
+  api.use('ground:minimax', ['client', 'server']);
   api.use('test-helpers', 'client');
   api.use(['tinytest', 'underscore', 'ejson']);
 
