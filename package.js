412c77486c1d2e9edd36a4698916d305d93be127
Morten Henriksen <mh@gi2.dk>
2014-12-17 17:05:15 +0100

    mbr update, remove versions.json

--- package.js
+++ package.js
@@ -5,7 +5,7 @@ Package.describe({
   git: "https://github.com/GroundMeteor/minimax.git"
 });
 
-Package.on_use(function (api) {
+Package.onUse(function (api) {
 
   // TODO: remove underscore deps _.each (used once)
 
@@ -16,19 +16,19 @@ Package.on_use(function (api) {
     'ejson'
   ], ['client', 'server']);
 
-  api.use(['ground:dictionary@0.1.0'], ['client', 'server']);
+  api.use(['ground:dictionary@0.1.1'], ['client', 'server']);
 
   api.export('MiniMax');
 
-  api.add_files('ejson.minimax.js', ['client', 'server']);
+  api.addFiles('ejson.minimax.js', ['client', 'server']);
 });
 
-Package.on_test(function (api) {
+Package.onTest(function (api) {
 
   api.use('ground:minimax', ['client', 'server']);
   api.use('test-helpers', 'client');
   api.use(['tinytest', 'underscore', 'ejson']);
 
-  api.add_files('ejson.minimax.tests.js', ['client', 'server']);
+  api.addFiles('ejson.minimax.tests.js', ['client', 'server']);
 
 });
