593bd1e9d4c1d3fcb436d757a500e40e1beb91d3
Morten Henriksen <mh@gi2.dk>
2014-12-17 20:57:41 +0100

    correct version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: "ground:minimax",
-  version: '1.0.1',
+  version: '1.0.2',
   summary: "Adds MiniMax.minify/maxify/parse/stringify making compress and decompress objects",
   git: "https://github.com/GroundMeteor/minimax.git"
 });
